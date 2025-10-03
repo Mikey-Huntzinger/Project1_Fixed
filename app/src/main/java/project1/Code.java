75882c7cf476e23c4b328c8a540c64e33013f577
Mikey-Huntzinger <mhuntzinger@csumb.edu>
2025-10-02 23:07:38 -0700

    Finished Reader.java

---
+++ app/src/main/java/project1/Code.java
@@ -0,0 +1,9 @@
+package project1;
+
+/**
+ * An enum to represent status codes for operations.
+ */
+public enum Code {
+    SUCCESS,
+    BOOK_ALREADY_CHECKED_OUT_ERROR, READER_COULD_NOT_REMOVE_BOOK_ERROR, READER_DOESNT_HAVE_BOOK_ERROR
+}
--- app/src/main/java/project1/Reader.java
+++ app/src/main/java/project1/Reader.java
@@ -1,5 +1,6 @@
 package project1;
 import java.util.List;
+// import project1.Code; // Removed because Code class does not exist or cannot be resolved
 
 public class Reader {
     public static int CARD_NUMBER_;
@@ -19,5 +20,83 @@ public class Reader {
         this.phone = phone;
     }
 
-    
+    public Code addBook(Book book){
+        if(this.books.contains(book)) {
+            return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
+        }
+        books.add(book);
+        return Code.SUCCESS;
+    }
+
+    public Code removeBook(Book book){
+        if (!this.books.contains(book)) {
+            return Code.READER_DOESNT_HAVE_BOOK_ERROR;
+        }
+        boolean removed = books.remove(book);
+        if (removed) {
+            return Code.SUCCESS;
+        } else {
+            return Code.READER_COULD_NOT_REMOVE_BOOK_ERROR;
+        }
+    }
+
+    public boolean hasBook(Book Book){
+        return books.contains(Book);
+    }
+
+    public int getBookCount(){
+        return books.size();
+    }
+
+    public List<Book> getBooks(){
+        return books;
+    }
+
+    public void setBooks(List<Book> books){
+        this.books = books;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(){
+        CARD_NUMBER_ = cardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(){
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Reader reader = (Reader) obj;
+        return cardNumber == reader.cardNumber &&
+               java.util.Objects.equals(name, reader.name) &&
+               java.util.Objects.equals(phone, reader.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(cardNumber, name, phone);
+    }
+
+    @Override
+    public String toString(){
+        return getName() + " (#" + getCardNumber() + ") has checked out " + getBooks();
+    }
 }
