901f30bd1c261db4f08337cea7eb46305ef3a800
Mikey-Huntzinger <mhuntzinger@csumb.edu>
2025-10-02 22:38:58 -0700

    Made Reader.java and static fields

---
+++ app/src/main/java/project1/Reader.java
@@ -0,0 +1,9 @@
+package project1;
+
+public class Reader {
+    public static int CARD_NUMBER_;
+    public static int NAME_;
+    public static int PHONE_;
+    public static int BOOK_COUNT_;
+    public static int BOOK_START_;
+}
