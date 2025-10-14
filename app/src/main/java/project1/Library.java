08e0034e7afef2d33e01255f576baa664df78364
Mikey-Huntzinger <mhuntzinger@csumb.edu>
2025-10-14 11:17:15 -0700

    init done

---
+++ app/src/main/java/project1/Library.java
@@ -0,0 +1,86 @@
+package project1;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import Utilities.Code;
+
+public class Library {
+    public static final int LENDING_LIMIT = 5;
+    private String name;
+    private static int libraryCard;
+    private List<Reader> readers;
+    private HashMap<String,Shelf> shelves;
+    private HashMap<Book,Integer> books;
+
+    //Constructor
+    public Library(String name){
+        this.name = name;
+        this.readers = new ArrayList<>();
+        this.shelves = new HashMap<>();
+        this.books = new HashMap<>();
+    }
+
+    public Code init(String filename){
+        File file = new File(filename);
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+        } catch (Exception FileNotFoundException) {
+            return Code.FILE_NOT_FOUND_ERROR;
+        }
+
+        //Now that file is initialized and found, we can read from it
+        //We can do books now
+        String bookCountString =scanner.nextLine();
+        int bookCount = convertInt(bookCountString, Code.BOOK_COUNT_ERROR);
+        if(bookCount < 0) {
+            return errorCode(bookCount);
+        }
+        Code bookResults = initBooks(bookCount, scanner);
+        if(bookResults != Code.SUCCESS){
+            scanner.close();
+            return bookResults;
+        }
+        listBooks();
+
+        //Now onto shelves
+        int shelfCount = convertInt(scanner.nextLine(), Code.SHELF_COUNT_ERROR);
+        if(shelfCount < 0){
+            return errorCode(shelfCount);
+        }
+        Code shelfResults = initShelves(shelfCount, scanner);
+        if(shelfResults != Code.SUCCESS){
+            return shelfResults;
+        }
+        listShelves();
+
+        //Now into readers
+        int readerCount = convertInt(scanner.nextLine(), Code.READER_COUNT_ERROR);
+        if(readerCount < 0){
+            return errorCode(readerCount);
+        }
+        Code readerResults = initReader(readerCount,scanner);
+        if(readerResults != Code.SUCCESS){
+            return readerResults;
+        }
+        listReaders();
+
+        scanner.close();
+        return Code.SUCCESS;
+    }
+
+    
+    private Code errorCode(int codeNumber) {
+        for (Code code : Code.values()) {
+        if (code.getCode() == codeNumber) {
+            return code;
+        }
+        }
+        return Code.UNKNOWN_ERROR;
+  }
+
+
+}
